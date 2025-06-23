60c06f0486f427dac5f8738358606a25040ccbd3
Tamura Yuji <uny.nippon@gmail.com>
2025-06-23 21:23:17 +0900

    Create Project

---
+++ ConnectFour/ConnectFour.Web/Services/FormFactor.cs
@@ -0,0 +1,16 @@
+using ConnectFour.Shared.Services;
+
+namespace ConnectFour.Web.Services;
+
+public class FormFactor : IFormFactor
+{
+    public string GetFormFactor()
+    {
+        return "Web";
+    }
+
+    public string GetPlatform()
+    {
+        return Environment.OSVersion.ToString();
+    }
+}
---
+++ ConnectFour/ConnectFour/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectFour;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
